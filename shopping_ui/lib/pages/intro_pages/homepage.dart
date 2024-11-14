e886f9f66c51ae6b6fb71379dba24adb523e6348
Dozie-dev <promiseamaechi16@gmail.com>
2024-11-14 09:00:30 +0100

    chore: search bar updated

--- shopping_ui/lib/pages/intro_pages/homepage.dart
+++ shopping_ui/lib/pages/intro_pages/homepage.dart
@@ -14,15 +14,20 @@ class Homepage extends StatelessWidget {
           actions: [
             Padding(
               padding: const EdgeInsets.only(right: 250.0, top: 10),
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.grey.withOpacity(0.15),
-                    borderRadius: BorderRadius.circular(20)),
-                child: IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.menu_rounded,
-                      size: 35,
+              child: InkWell(
+                onTap: () {},
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.grey.withOpacity(0.15),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Image.asset(
+                        'icons/menu.png',
+                        height: 25,
+                        width: 25,
+                      ),
                     )),
               ),
             ),
@@ -36,16 +41,19 @@ class Homepage extends StatelessWidget {
                     onPressed: () {},
                     icon: Icon(
                       Icons.notifications_active_outlined,
-                      size: 35,
+                      size: 38,
                     )),
               ),
             ),
           ],
         ),
+
+        //The body of the page
         body: Column(
           children: [
+            //The search bar
             Padding(
-              padding: EdgeInsets.all(10.0),
+              padding: EdgeInsets.all(25.0),
               child: Container(
                   height: 60,
                   decoration: BoxDecoration(
@@ -59,16 +67,29 @@ class Homepage extends StatelessWidget {
                           Icons.search,
                           size: 30,
                         ),
-                        //VerticalDivider(color: Colors.grey,),
-
                         hintText: "Search...",
                         hintStyle: TextStyle(color: Colors.grey, fontSize: 25),
-                        suffixIcon: Icon(
-                          Icons.open_in_browser_rounded,
-                          size: 30,
-                        )),
+                        suffixIcon:
+                            Row(mainAxisSize: MainAxisSize.min, children: [
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(top: 15.0, bottom: 15),
+                            child: VerticalDivider(
+                              color: Colors.grey,
+                              width: 3,
+                              thickness: 2,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Image.asset('icons/list.png'),
+                          )
+                        ])),
                   )),
-            )
+            ),
+
+            //--Container after the search
+            
           ],
         ));
   }
